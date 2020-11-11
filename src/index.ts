a5686091371ceff16cb210561b9207a1e0a15ce3
Panos-JC <phatziioannou@gmail.com>
2020-11-11 11:08:53 +0200

    UPDATE: ubstract typeorm connection, secure cookie

--- src/index.ts
+++ src/index.ts
@@ -5,20 +5,11 @@ import cors from "cors";
 import express from "express";
 import session from "express-session";
 import Redis from "ioredis";
-import path from "path";
+// import path from "path";
 import "reflect-metadata";
 import { buildSchema } from "type-graphql";
 import { createConnection } from "typeorm";
 import { COOKIE_NAME, __prod__ } from "./constants";
-import { Admin } from "./entities/Admin";
-import { Expertise } from "./entities/Expertise";
-import { Individual } from "./entities/Individual";
-import { Industry } from "./entities/Industry";
-import { Mentor } from "./entities/Mentor";
-import { Review } from "./entities/Review";
-import { Skill } from "./entities/Skill";
-import { Users } from "./entities/Users";
-import { WorkExperience } from "./entities/WorkExperience";
 import { AdminResolver } from "./resolvers/admin";
 import { ExpertiseResolver } from "./resolvers/expertise";
 import { IndividualResolver } from "./resolvers/individual";
@@ -28,38 +19,11 @@ import { ReviewResolver } from "./resolvers/review";
 import { SkillResolver } from "./resolvers/skill";
 import { UsersResolver } from "./resolvers/user";
 import { WorkExperienceResolver } from "./resolvers/workExperience";
-import { SessionRequest } from "./entities/SessionRequest";
 import { SessionRequestResolver } from "./resolvers/sessionRequest";
-import { Company } from "./entities/Company";
 import { CompanyResolver } from "./resolvers/company";
 
 const main = async () => {
-  await createConnection({
-    type: "postgres",
-    // database: "feleatest2",
-    // username: "postgres",
-    // password: "postgres",
-    url: process.env.DATABASE_URL,
-    logging: !__prod__,
-    synchronize: !__prod__,
-    entities: [
-      Users,
-      Mentor,
-      Individual,
-      Admin,
-      WorkExperience,
-      Industry,
-      Skill,
-      Expertise,
-      Review,
-      SessionRequest,
-      Company,
-    ],
-    migrations: [path.join(__dirname, "/migrations/*")],
-    migrationsRun: true,
-  });
-
-  // await conn.runMigrations();
+  await createConnection();
 
   // express app
   const app = express();
@@ -86,7 +50,7 @@ const main = async () => {
         maxAge: 1000 * 60 * 60 * 24, // 24 hours
         httpOnly: false,
         sameSite: "lax",
-        secure: false, // __prod__, // cookie only works in https
+        secure: true, // __prod__, // cookie only works in https
       },
       saveUninitialized: false,
       secret: "thisisasecret", // Should be hidden
