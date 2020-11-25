44b09b6488fe8ca891a06e2b8c63fcc6ac91c1a5
Panos-JC <phatziioannou@gmail.com>
2020-11-25 15:06:56 +0200

    UPDATE: work experience responses

--- src/resolvers/workExperience/workExperience.response.ts
+++ src/resolvers/workExperience/workExperience.response.ts
@@ -2,6 +2,15 @@ import { ObjectType, Field } from "type-graphql";
 import { WorkExperience } from "../../entities/WorkExperience";
 
 @ObjectType()
+export class WorkExperiencesResponse {
+  @Field(() => String, { nullable: true })
+  errorMsg?: string;
+
+  @Field(() => [WorkExperience], { nullable: true })
+  data?: WorkExperience[];
+}
+
+@ObjectType()
 export class WorkExperienceResponse {
   @Field(() => String, { nullable: true })
   errorsMsg?: string;
