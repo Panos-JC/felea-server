dfaeef4e427b0a4b253855046fa65ce32d68c09a
Panos-JC <phatziioannou@gmail.com>
2020-11-30 16:23:06 +0200

    ADD: email templates

---
+++ src/utils/send.ts
@@ -0,0 +1,53 @@
+import nodemailer from "nodemailer";
+import path from "path";
+
+const hbs = require("nodemailer-express-handlebars");
+
+const hbsConfig = {
+  viewEngine: {
+    extName: ".hbs",
+    partialsDir: path.join(__dirname, "../email_templates/"),
+    layoutsDir: path.join(__dirname, "../email_templates/"),
+    defaultLayout: "",
+  },
+  viewPath: path.join(__dirname, "../email_templates/"),
+  extName: ".hbs",
+};
+
+export async function send(
+  to: string,
+  subject: string,
+  template: string,
+  context: any
+) {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  console.log("[TEST]: ", process.env.EMAIL_USER);
+  console.log("[TEST]: ", process.env.EMAIL_PASS);
+
+  transporter.use("compile", hbs(hbsConfig));
+
+  const email = {
+    from: `"Felea Bot" <${process.env.EMAIL_USER}>`,
+    to,
+    // cc: ccValues,
+    subject: subject,
+    template: template,
+    context: context,
+  };
+
+  await transporter.sendMail(email);
+  // {
+  //   from: '"Felea" <infopanostest@gmail.com>', // sender address
+  //   to, // list of receivers
+  //   subject: "SUBJECT", // Subject line
+  //   text: html, // plain text body
+  //   html, // html body
+  // }
+}
